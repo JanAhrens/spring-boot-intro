2c9effe0c057ccf2c032a1f384e0fcc7b5fbcf56
Jan Ahrens <jan.ahrens@moovel.com>
2016-05-18 07:14:00 +0200

    Replace example01 with example02

--- example01/src/main/java/io/github/janahrens/hellospring/Example01Application.java
+++ example01/src/main/java/io/github/janahrens/hellospring/Example01Application.java
@@ -1,30 +1,26 @@
 package io.github.janahrens.hellospring;
 
-import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-
-import javax.annotation.PostConstruct;
+import org.springframework.context.annotation.Bean;
+import ratpack.handling.Handler;
+import ratpack.spring.config.EnableRatpack;
+import ratpack.spring.config.RatpackProperties;
 
 @SpringBootApplication
+@EnableRatpack
 public class Example01Application {
+  /**
+   * The Ratpack application uses configurations from the {@link RatpackProperties}.
+   *
+   * @return a configured Ratpack {@link Handler}
+   */
+  @Bean
+  public Handler handler() {
+    return ctx -> ctx.render("Hello World.");
+  }
 
-	private final Logger log = Logger.getLogger(Example01Application.class);
-
-    @Autowired
-    ApplicationContext applicationContext;
-
-    /**
-     * Provides a way to look at the state of the {@link ApplicationContext} after dependency injection has been done.
-     */
-	@PostConstruct
-	private void init() {
-		log.info("Hello World! There are " + applicationContext.getBeanDefinitionCount() + " beans defined.");
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(Example01Application.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(Example01Application.class, args);
+  }
 }
